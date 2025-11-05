5def2db64bec81f747a12863c1a5c24d227f170d
Chrisx-39 <57959816+Chrisx-39@users.noreply.github.com>
2025-11-06 01:16:33 +0200

    Update settings.py

--- hostelmanager/settings.py
+++ hostelmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^$m_obcw$6%o7l!4tw&shz8kpwooxa(4ryttk$8sj&y5n@g7y%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*' ]
 
 
 # Application definition
