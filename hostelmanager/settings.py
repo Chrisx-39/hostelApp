5574dc262e08606ad34adf00ad92f42b874946b1
Chrisx-39 <57959816+Chrisx-39@users.noreply.github.com>
2025-11-06 18:39:00 +0200

    results

--- hostelmanager/settings.py
+++ hostelmanager/settings.py
@@ -137,6 +137,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
