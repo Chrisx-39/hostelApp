6d80b15f6cfd4ba3e570b56333a3ce35a8215bc4
Chrisx-39 <57959816+Chrisx-39@users.noreply.github.com>
2025-11-06 17:47:33 +0200

    results

--- hostelmanager/settings.py
+++ hostelmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +79,13 @@ WSGI_APPLICATION = 'hostelmanager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://neondb_owner:npg_OHZWhC3v2UpD@ep-dawn-resonance-ah1feo8d-pooler.c-3.us-east-1.aws.neon.tech/hosteldb?sslmode=require&channel_binding=require",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
